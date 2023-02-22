2f6be5fb61a99bfc435f0e7592297c1cb6b50fb7
John Doe <shellbd22@gmail.com>
2023-02-22 08:35:30 +0700

    onback

---
+++ README.md
@@ -0,0 +1,3 @@
+Completely refactored the usage of it with ox_inventory
+
+I updated it to use listeners instead of having to edit the inventory items, making this basically drag and drop, this is only dependent on ox_lib and ox_inventory
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+--[[ FX Information ]]--
+fx_version   'cerulean'
+lua54        'yes'
+game         'gta5'
+
+
+name         'g0ogle'
+version      '0.0.0'
+author       'gns'
+description   'g0ogle'
+
+shared_script '@ox_lib/init.lua'
+
+client_scripts {
+	'client.lua',
+}
